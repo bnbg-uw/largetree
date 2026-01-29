26695003c620cb4eddac46e0e1c8ab279b82de27
bnbg@uw.edu <bnbg@uw.edu>
2026-01-29 14:51:07 -0800

    committing un tracked files

--- src/largetree.cpp
+++ src/largetree.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include "ReadProcessedFolder.hpp"
 #include "taolist.hpp"
+#include "models.hpp"
+#include "graphlico.hpp"
 #include <boost/program_options.hpp>
 #include <chrono>
 
@@ -63,7 +65,7 @@ int main(int argc, char* argv[])
     std::unique_ptr<processedfolder::ProcessedFolder> lds;
     lds = processedfolder::readProcessedFolder(vm["lidar"].as<std::string>());
     std::cout << "LDS made...";
-    
+
     auto maskr = lapis::Raster<int>(processedfolder::stringOrThrow(lds->maskRaster()));
     if (vm.count("mask")) {
         auto m = lapis::Raster<int>(vm["mask"].as<std::string>());
@@ -79,14 +81,14 @@ int main(int argc, char* argv[])
     }
     catch (lapis::InvalidRasterFileException e) {
         std::cerr << "Unable to load climate layer from specified file";
-        return 1; 
+        return 1;
     }
 
     auto output = lds->dir();
     if (std::filesystem::exists(output)) {
         output = output / "largetree";
         std::filesystem::create_directory(output);
-    } 
+    }
     else {
         throw std::runtime_error("Output path does not exist.");
     }
@@ -97,38 +99,34 @@ int main(int argc, char* argv[])
     }
     double fixed = 3;
 
-    std::cout << "Reading TAOs\n";
-    auto before = std::chrono::high_resolution_clock::now();
-    auto tl = rxtools::TaoListMP(lds->allHighPoints(nthread, fixed),);
-
-    auto after = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::seconds>(after - before);
-    print(duration.count());
-
     // Done loading input stuff
     //starting fia models
 
     std::string fiafolder = vm["fia"].as<std::string>();
-    auto allPlots = stats::FIALeastSquares::getPlotList(fiafolder);
 
-    lapis::coord_t buffer = 2000 / lds->getConvFactor();
-    lapis::Extent bufferext = lapis::Extent(maskr.xmin() - buffer, maskr.xmax() + buffer, maskr.ymin() - buffer, maskr.ymax() + buffer);
-    bufferext.crs(maskr.crs());
+    auto reader = rxtools::allometry::FIAReader(fiafolder);
 
-    stats::FIALeastSquares basedbh = stats::FIALeastSquares(allPlots, fiafolder, "DIA");
-    std::size_t plotcount = basedbh.limitByExtent(bufferext);
-    if (plotcount == 0) {
+    auto buffer = lds->units().value().convertOneToThis(10000, lapis::linearUnitPresets::meter);
+    lapis::Extent e = lapis::Extent(maskr.xmin() - buffer, maskr.xmax() + buffer, maskr.ymin() - buffer, maskr.ymax() + buffer, maskr.crs());
+    if (!reader.limitByExtent(e)) {
         std::cerr << "No FIA plots within vicinity of acquisition. Cannot calculate basal area or biomass.\n";
         exit(EXIT_FAILURE);
     }
-    std::cout << "Class-Agnostic:\n";
-    std::cout << "DBH:\n";
-    basedbh.initializeModel();
-    std::cout << basedbh << "\n";
+
+    reader.makePlotTreeMap(std::vector<std::string>{ "DIA", "DRYBIO_AG" });
+    auto allTrees = reader.collapsePlotTreeMap();
+    auto dbhModel = rxtools::allometry::UnivariateLinearModel(allTrees, "DIA", rxtools::linearUnitPresets::inch);
+    auto bioModel = rxtools::allometry::UnivariateLinearModel(allTrees, "DRYBIO_AG", lapis::linearUnitPresets::unknownLinear);
+    std::cout << " Class-agnostic:!\n";
+    std::cout << dbhModel << "\n";
+    std::cout << bioModel << "\n";
+
     //climate individual models.
-    std::unordered_map<int, stats::FIALeastSquares> dbhByClim;
+    std::unordered_map<int, rxtools::allometry::UnivariateLinearModel> dbhByClim;
+    std::unordered_map<int, rxtools::allometry::UnivariateLinearModel> bioByClim;
     std::unordered_set<int> checkedValues;
 
+    size_t plotcount = 0;
     for (lapis::cell_t cell = 0; cell < climate.ncell(); ++cell) {
         auto v = climate[cell];
         if (!v.has_value()) {
@@ -139,16 +137,42 @@ int main(int argc, char* argv[])
         }
         checkedValues.insert(v.value());
 
-        stats::FIALeastSquares thisDbh = basedbh;
-        plotcount = thisDbh.limitByRasterValue(climate, v.value());
+        auto thisReader = reader;
+        plotcount = thisReader.limitByRasterValue(climate, v.value());
         if (plotcount > 0) {
-            dbhByClim.emplace(v.value(), thisDbh);
+            auto thisTrees = reader.collapsePlotTreeMap();
+            auto thisdbh = rxtools::allometry::UnivariateLinearModel(allTrees, "DIA", rxtools::linearUnitPresets::inch);
+            auto thisbio = rxtools::allometry::UnivariateLinearModel(allTrees, "DRYBIO_AG", lapis::linearUnitPresets::unknownLinear);
+            dbhByClim.emplace(v.value(), thisdbh);
+            bioByClim.emplace(v.value(), thisbio);
             std::cout << "Class " << v.value() << ":\n";
             std::cout << "DBH:\n";
-            dbhByClim[v.value()].initializeModel();
             std::cout << dbhByClim[v.value()] << "\n";
+            std::cout << "BIO:\n";
+            std::cout << bioByClim[v.value()] << "\n";
         }
     }
+
+    std::cout << "Reading TAOs\n";
+    auto before = std::chrono::high_resolution_clock::now();
+    rxtools::TaoGettersMP getters = rxtools::TaoGettersMP(
+        lapis::lico::alwaysAdd<lapis::VectorDataset<lapis::MultiPolygon>>,
+        lds->coordGetter(),
+        lds->heightGetter(),
+        lds->radiusGetter(),
+        lds->areaGetter(),
+        //TODO: at some point it would be good to not implicitly assume height units are not in meters.
+        [dm = dbhModel, hg = lds->heightGetter()](const lapis::ConstFeature<lapis::MultiPolygon>& ft)->double {
+            return dm.predict(hg(ft), lapis::linearUnitPresets::meter, rxtools::linearUnitPresets::centimeter);
+        }
+    );
+    auto tl = rxtools::TaoListMP(lds->allPolygons(), getters);
+
+    auto after = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::seconds>(after - before);
+    print(duration.count());
+
+
     std::cout << "making layers\n";
 
     lapis::Raster<int> ntree{ lapis::Alignment(maskr) };
@@ -166,21 +190,21 @@ int main(int argc, char* argv[])
     f.open("F:/trees.csv");
     auto projclim = lapis::resampleRaster(climate, maskr, lapis::ExtractMethod::near);
     for (size_t t = 0; t < tl.size(); ++t) {
-        auto x = tl.x()[t];
-        auto y = tl.y()[t];
-        auto h = tl.height()[t];
+        auto x = tl.x(t);
+        auto y = tl.y(t);
+        auto h = tl.height(t);
         auto thisClim = projclim.extract(x, y, lapis::ExtractMethod::near);
         double dbh = 0;
         if (thisClim.has_value()) {
             if (dbhByClim.count(thisClim.value())) {
-                dbh = dbhByClim[thisClim.value()].predictAsMetric(h);
+                dbh = dbhByClim[thisClim.value()].predict(h, lapis::linearUnitPresets::meter);
             }
             else {
-                dbh = basedbh.predictAsMetric(h);
+                dbh = dbhModel.predict(h, lapis::linearUnitPresets::meter);
             }
         }
         else {
-            dbh = basedbh.predictAsMetric(h);
+            dbh = dbhModel.predict(h, lapis::linearUnitPresets::meter);
         }
 
         if (!maskr.contains(x, y)) {
@@ -194,12 +218,12 @@ int main(int argc, char* argv[])
         ntree.atRC(row, col).has_value() = true;
         ntree.atRC(row, col).value()++;
         totarea.atRC(row, col).has_value() = true;
-        totarea.atRC(row, col).value() += tl.area()[t];
+        totarea.atRC(row, col).value() += tl.area(t);
         ltree.atRC(row, col).has_value() = true;
         if (dbh > dia) {
             ltree.atRC(row, col).value()++;
             area.atRC(row, col).has_value() = true;
-            area.atRC(row, col).value() += tl.area()[t];
+            area.atRC(row, col).value() += tl.area(t);
         }
 
         for (int rowbudge : {-1, 0, 1}) {
@@ -215,12 +239,12 @@ int main(int argc, char* argv[])
                 ntree90.atRC(thisRow, thisCol).has_value() = true;
                 ntree90.atRC(thisRow, thisCol).value()++;
                 totarea90.atRC(row, col).has_value() = true;
-                totarea90.atRC(row, col).value() += tl.area()[t];
+                totarea90.atRC(row, col).value() += tl.area(t);
                 ltree90.atRC(thisRow, thisCol).has_value() = true;
                 if (dbh > dia) {
                     ltree90.atRC(thisRow, thisCol).value()++;
                     area90.atRC(row, col).has_value() = true;
-                    area90.atRC(row, col).value() += tl.area()[t];
+                    area90.atRC(row, col).value() += tl.area(t);
                 }
             }
         }
@@ -284,7 +308,7 @@ int main(int argc, char* argv[])
         }
 
         auto cc = lapis::connectedComponents(patch, false);
-        std::unordered_map<int, int> regionType;
+        std::unordered_map<lapis::cell_t, int> regionType;
         for (lapis::cell_t c = 0; c < cc.ncell(); ++c) {
             if (cc[c].has_value()) {
                 regionType[cc[c].value()]++;
